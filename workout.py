bf72fd68603e2f9a8663b4afbdfa3d07e8a8a0b0
Jeongkyun Jin <jeung9709@gmail.com>
2020-04-29 12:22:08 +0900

    Added context menu, a bit of refactoring

--- workout.py
+++ workout.py
@@ -8,6 +8,7 @@ import sys
 from PyQt5 import QtWidgets, QtCore, QtGui
 from PyQt5.QtCore import QTimer, QTime
 
+
 PARTS = ['Upper', 'Lower', 'Torso', 'Core']
 TYPE = ['Reps', 'Secs']
 
@@ -20,8 +21,6 @@ default_workouts = [
 ]
 default_names = [workout[0] for workout in default_workouts]
 
-print(default_names)
-
 # Save database initialize
 def createTable():
     # Create table and fill it with default workouts if it is empty
@@ -31,7 +30,7 @@ def createTable():
         cursor.execute('INSERT INTO workoutdata(name, part, type, minreps, difficulty, curReps, overHeat, totalAlerts) VALUES(?, ?, ?, ?, ?, ?, ?, ?)', 
         (workout[0], workout[1], workout[2], workout[3], workout[4], workout[3], 0, 0))
 
-
+# If the database exists, connect. Else, create the table.
 if os.path.exists('workoutdata.db'):
     conn = sqlite3.connect('workoutdata.db', isolation_level=None)
     cursor = conn.cursor()
@@ -40,16 +39,13 @@ else:
     cursor = conn.cursor()
     createTable()
 
-
 def retrieveData():
-    cursor.execute('SELECT ID FROM workoutdata')
-    return cursor.fetchall()
+    cursor.execute('SELECT * FROM workoutdata')
+    load_workout_tuple = cursor.fetchall()
+    load_workout = [list(tuples) for tuples in load_workout_tuple]
+    return load_workout
 
-cursor.execute('SELECT * FROM workoutdata')
-load_workout_tuple = cursor.fetchall()
-load_workout = [list(tuples) for tuples in load_workout_tuple]
-
-print(load_workout)
+load_workout = retrieveData()
 
 # Calculate Resting time
 def calculateRest():
@@ -72,41 +68,58 @@ def calculateReps(row):
         return row
 
 # Show Alert
-def ShowAlert():
-    # Randomly pick a workout
-    workout_index = random.randrange(0, len(retrieveData()))
+def ShowAlert(command="alert"):
+    if command == 'list':
+        ctypes.windll.user32.MessageBoxW(0, str(load_workout), 1)
+        print(str(load_workout))
+    elif command == 'alert':
+        # Randomly pick a workout
+        workout_index = random.randrange(0, len(retrieveData()))
 
-    # Show an alert
-    #ctypes.windll.user32.MessageBoxW(0, math.floor(str(load_workout[workout_index][6])), str(load_workout[workout_index][1]), 1)
-    ctypes.windll.user32.MessageBoxW(0, str(math.floor(load_workout[workout_index][6])), str(load_workout[workout_index][1]), 1)
+        # Show an alert
+        #ctypes.windll.user32.MessageBoxW(0, math.floor(str(load_workout[workout_index][6])), str(load_workout[workout_index][1]), 1)
+        ctypes.windll.user32.MessageBoxW(0, str(math.floor(load_workout[workout_index][6])), str(load_workout[workout_index][1]), 1)
 
-    # Update Reps
-    load_workout[workout_index] = calculateReps(load_workout[workout_index])
+        # Update Reps
+        load_workout[workout_index] = calculateReps(load_workout[workout_index])
 
-    # Apply to the database
-    # cursor.execute("UPDATE workoutdata SET curReps=?, totalAlerts=? WHERE ID=?", (load_workout[workout_index][6], load_workout[workout_index][8], load_workout[workout_index][0]))
+        # Apply to the database
+        cursor.execute("UPDATE workoutdata SET curReps=?, totalAlerts=? WHERE ID=?", (load_workout[workout_index][6], load_workout[workout_index][8], load_workout[workout_index][0]))
+        
 
 # Tray Icon Implement
 class SystemTrayIcon(QtWidgets.QSystemTrayIcon):
-
+    """
+    GUI CLASS FOR WORKOUT ALERT
+    Author: ORIGINAL CODE FROM THE INTERWEBS EDITED BY JIN JEONGKYUN
+    Date: 2020-04-29
+    """
     def __init__(self, icon, parent=None):
         QtWidgets.QSystemTrayIcon.__init__(self, icon, parent)
-        menu = QtWidgets.QMenu(parent)
-        exitAction = menu.addAction("Exit")
-        
+
         self.timer = QTimer(self)
-        self.timer.setInterval(1000)
+        self.timer.setInterval(2000000)
         self.timer.start()
         self.timer.timeout.connect(self.timeout)
+
+        menu = QtWidgets.QMenu(parent)
+
+        exitAction = menu.addAction("Exit")
+        exitAction.triggered.connect(lambda: exit())
+
+        viewWorkouts = menu.addAction("View Workout List")
+        viewWorkouts.triggered.connect(lambda: ShowAlert('list'))
+
+        showTimeLeft = menu.addAction("Show Time Left")
+        showTimeLeft.triggered.connect(lambda: ctypes.windll.user32.MessageBoxW(0, str(math.floor(self.timer.remainingTime()/1000)) + " SECS", 1))
+        
         self.setContextMenu(menu)
-        menu.triggered.connect(self.exit)
 
     def exit(self):
         QtCore.QCoreApplication.exit()
 
     def timeout(self):
         sender = self.sender()
-        currentTime = QTime.currentTime().toString("hh:mm:ss")
  
         if id(sender) == id(self.timer):
             ShowAlert()
@@ -119,5 +132,8 @@ def main(image):
     sys.exit(app.exec_())
 
 if __name__ == '__main__':
-    on='icon.ico'
+    try:
+        on='icon.ico'
+    except Exception as e:
+        ctypes.windll.user32.MessageBoxW(0, str(e) + 'Icon error. Make sure icon.ico file is in the same directory.', 1)
     main(on)
